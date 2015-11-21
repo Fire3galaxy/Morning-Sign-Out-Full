d7d8255c746ddd6b205f14c9473d65be8c1d6df2
Fire3galaxy <d.handojo2013@live.com>
2015-11-21 14:14:22 -0800

    Fragments
    Using both listview and webview as fragments of a single activity,
    deciding which one to launch based on a json request to check if that
    person had articles. Rejected because too complicated.

--- Morning-Sign-Out/app/src/main/java/app/morningsignout/com/morningsignoff/MTTListViewActivity.java
+++ Morning-Sign-Out/app/src/main/java/app/morningsignout/com/morningsignoff/MTTListViewActivity.java
@@ -1,7 +1,10 @@
 package app.morningsignout.com.morningsignoff;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -19,15 +22,15 @@ public class MTTListViewActivity extends ActionBarActivity {
         setContentView(R.layout.activity_mttwebview);
 
         ListView team = (ListView) findViewById(R.id.listView_mtt);
-        team.setAdapter(new TeamRowAdapter(null));
+        team.setAdapter(new TeamRowAdapter(this, null));
     }
 }
 
 class TeamRowAdapter extends BaseAdapter {
-    List<String> hyperlinks;
+    Context c;
 
-    public TeamRowAdapter(List<String> hyperlinks) {
-        this.hyperlinks = hyperlinks;
+    public TeamRowAdapter(Context c) {
+        this.c = c;
     }
 
     @Override
@@ -47,7 +50,20 @@ class TeamRowAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // FIXME: Change this to load my team_row xml.
-        return null;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) c.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.team_row, parent, false);
+        }
+
+        // FIXME: make asynctask that loads all of data and places them into array of TeamRowObjects
+        // to update the adapter.
+        // Adapter should only be in charge of putting data into views. (Remember to store views in viewHolder pattern
+        // The asynctask should be responsible for getting all data and should be called in onCreate() once for simplicity.
+        // It should also be decided that if the initially called user has articles, the webview should be used, not this
+        // listview. Possibility: this activity could really be a fragment. The activity makes a first call, and decides
+        // which fragment to load. Issue: Internet to decide. Slow. Could use progressbar
+
+        // Idea 2: Scrap this idea with JSON extra requests. Use just the webview. Add buttons to fill in whitespace (vist url, email)
+        // issue: lots. Eg. what if he doesn't have email/url, what if he has articles (do buttons still show at bottom)>
     }
 }
